0ab1fa09c2d8650ebea85880b4fe64eb83006482
Brian Hague <alphaseinor@gmail.com>
2019-11-21 20:30:49 -0600

    added creators to actions with console logs only

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const ADD_FEATURE = 'ADD_FEATURE'
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+
+//creators
+
+// ADD_FEATURE
+
+export const addFeature = (feature) => {
+  console.log(feature)
+}
+
+// REMOVE_FEATURE
+
+export const removeFeature = (feature) => {
+  console.log(feature)
+}
